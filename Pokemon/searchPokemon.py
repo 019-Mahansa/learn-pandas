adcf7c1871267adf3a589143613ef1a0849bf704
019-Mahansa <lightningplay2009@gmail.com>
2026-02-09 12:48:48 +0700

    firs-in-new-branch

--- Pokemon/searchPokemon.py
+++ Pokemon/searchPokemon.py
@@ -1,11 +1,38 @@
 import pandas as pd
+df = pd.read_csv("data.csv", index_col="Type1")
 
-df = pd.read_csv("data.csv", index_col="Name")
 
-pokemon = input("Ketik nama pokemon yang ingin anda cari: ")
-upper = pokemon.capitalize()
+def mainMenu():
+    print("Welcome to Pokemon menu!")
+    print("1. Type of Pokemon")
+    print("2. Nama Pokemon")
+    print("3. Legendary Character \n")
+    pokemon = int(input("Pilih menu yang anda inginkan : "))
+    if pokemon == 1:
+        pokemonType()
+    elif pokemon == 2:
+        pokemonName()
+    elif pokemon == 3:
+        pokemonLegendary()
+    else:
+        print("Pencarian anda gagal!")
 
-try:
-    print(df.loc[upper])
-except KeyError:
-    print(f" {upper} tidak di temukan!")
+def pokemonType():
+
+    print("Welcome to type of pokemon!")
+    ine = input("typing type pokemon that you want: ")
+    print(df["Rock"])
+    print("That is all type of pokemon!")
+
+def pokemonName():
+    print("1")
+
+def pokemonLegendary():
+    print("1")
+
+mainMenu()
+
+# try:
+#     print(df.loc[upper])
+# except KeyError:
+#     print(f" {upper} tidak di temukan!")
